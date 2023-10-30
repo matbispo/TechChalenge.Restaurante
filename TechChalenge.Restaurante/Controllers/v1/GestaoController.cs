7947050b6dc44bf920327cc341e9a5d8d5fc8509
Mateus Bispo <math.l.bispo@gmail.com>
2023-10-30 15:12:50 -0300

    Adicionar arquivos de projeto.

---
+++ TechChalenge.Restaurante/Controllers/v1/GestaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechChalenge.Restaurante.Controllers.v1
+{
+    public class GestaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TechChalenge.Restaurante/Controllers/v1/PedidoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechChalenge.Restaurante.Controllers.v1
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
